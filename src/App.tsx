06a7c69b420ad65720f7e2187be646f917d01f9b
vatl1x <vadimkul321@yandex.ru>
2026-03-20 21:56:06 +0300

    add context and change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header/Header";
+import { useTheme } from "./context/ThemeContext";
 import Main from "./pages/Main/Main";
 
 function App() {
+    const {isDark} =useTheme()
     return (
-        <>
+        <div className={`app ${isDark ? "dark" : "light"}`}>
             <Header />
             <div className="container">
                 <Main />
             </div>
-        </>
+        </div>
     );
 }
 
---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import dark from "./theme/dark.svg";
+import light from "./theme/light.svg";
+
+export const themeIcons = {
+    dark,
+    light,
+};
